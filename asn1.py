16c9cadc6ca49653f4a92be7ef35410fb09c9abe
Bhaskar-iot <57404381+Bhaskar-iot@users.noreply.github.com>
2020-03-07 05:28:48 +0530

    output to text file and connect to mysql

--- asn1.py
+++ asn1.py
@@ -1,4 +1,4 @@
-
+import mysql.connector as mq
 from os.path import exists
 from sys import argv
 
@@ -6,13 +6,22 @@ from sys import argv
 class Protein:
     name = ''
     seq = ''
+    err = 0
 
     def __init__(self, nm, sec):
         self.name = nm
         self.seq = sec
+        self.err_check()
+
+    def err_check(self):
+        seq_length = len(self.seq)
+        if (seq_length % 3) == 0:
+            self.err = 0
+        else:
+            self.err = 1
 
     def get_data(self):
-        return self.name, self.seq
+        return self.name, self.seq, self.err
 
 
 def read_protein(fname):
@@ -42,6 +51,13 @@ def read_protein(fname):
 
 
 def main():
+    mydb = mq.connect(host="localhost", user="root", passwd="L4qjg0ss", database="proteins")
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SHOW TABLES")
+
+    for x in mycursor:
+        print(x)
     fname = argv[1]
     if not exists(fname):
         print("Error: File does not exist")
@@ -50,11 +66,10 @@ def main():
     else:
         plist = read_protein(fname)
         l = len(plist)
-        for i in range(0, l):
-            nm=plist[i].name
-            sq=plist[i].seq
-            print(plist[i].name, plist[i].seq)
-           # k=input()
+        fr=open('out.txt', 'w')
+        fr.write('name,sequence,error\n')
+        for i in range(0,l):
+            fr.write('%s,%s,%s\n' % (plist[i].name, plist[i].seq, plist[i].err))
         exit(0)
 
 
