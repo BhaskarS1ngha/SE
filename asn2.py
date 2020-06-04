46b251440203067970dde707ff311f3616f3fe95
bhaskar-IoT <bhaskarsingha7@gmail.com>
2020-06-05 03:00:31 +0530

    amino acid seq created

--- asn2.py
+++ asn2.py
@@ -18,19 +18,34 @@ codonDict = {
     'stop' :['taa' ,'tag' ,'tga'],
     'his' :['cat', 'cac'],
     'gin': ['caa', 'cag'],
-    'asn': ['aat' ,'aac'],
+    'asn': ['aat', 'aac'],
     'lys': ['aaa', 'aag'],
-    'asp': ['gat' ,'gac'],
-    'glu': ['gaa' ,'gag'],
+    'asp': ['gat', 'gac'],
+    'glu': ['gaa', 'gag'],
     'cys': ['tgt', 'tgc'],
     'trp': ['tgg'],
     'arg': ['cgt', 'cgc', 'cga', 'cgg', 'aga', 'agg'],
-    'gly': ['ggt', 'ggc', 'gga','ggg']
+    'gly': ['ggt', 'ggc', 'gga', 'ggg']
 }
 
 
-def get_amino_acid(codon):
+def get_amino_acid_notn(amino_acid):
+    amino_dict = read_amino_acids("aminoAcids.txt")
+    key_list = amino_dict.keys()
+    for key in key_list:
+        temp_notation = amino_dict[key]
+        for x in temp_notation:
+            if x == amino_acid:
+                return key
 
+
+def get_amino_acid(codon):
+    key_list = codonDict.keys()
+    for key in key_list:
+        temp_codon = codonDict[key]
+        for x in temp_codon:
+            if x == codon:
+                return get_amino_acid_notn(key)
 
 def connect_to_database():
     mydb = mq.connect(host="localhost", user="root", passwd="L4qjg0ss", database="SE")
@@ -74,14 +89,14 @@ def update_gen_len():
     #print(mycursor.rowcount)
 
 
-def get_amino_acid(gen_seq, len_gene):
-    mydict = read_amino_acids("aminoAcids.txt")
+def get_amino_acid_seq(gen_seq, len_gene):
     i = 0
     amino_seq = ''
-    while i<len_gene:
-        codon = gen_seq[i:i+3]
-        i+=3
-        print(mydict[codon])
+    while i < len_gene:
+        codon = gen_seq[i:i + 3]
+        i += 3
+        amino_seq += get_amino_acid(codon)
+    return amino_seq
 
 
 
@@ -94,13 +109,14 @@ def update_amino_acid():
     for row in result:
         if row[2] != 0:
             seq = row[1]
-            gene_len= row[3]
-#            get_amino_acid(seq,gene_len)
-            #aminoSeq = get_amino_acid(seq)
+            gene_len = row[3]
+            aminoSeq = "\"" + get_amino_acid_seq(seq, gene_len) + "\""
+            sql = "update proteins set aminoAcidSequence = %s where name = %s "
+            val = [aminoSeq, row[0]]
+            mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
     mydb.close()
-
-
-
-
 
 update_amino_acid()
